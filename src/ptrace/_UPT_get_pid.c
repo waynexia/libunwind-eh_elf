da7446a21ab4177d45b72da851a985903e9ee456
Théophile Bastian <contact@tobast.fr>
2018-05-31 15:45:07 +0200

    Add get_pid in ptrace support

---
+++ src/ptrace/_UPT_get_pid.c
@@ -0,0 +1,20 @@
+/********** Libunwind -- eh_elf flavour **********
+ * This is the eh_elf version of libunwind, made for academic purposes.
+ *
+ * Théophile Bastian <theophile.bastian@ens.fr> <contact+github@tobast.fr>
+ *************************************************
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ ************************************************/
+
+#include "_UPT_internal.h"
+
+int _UPT_get_pid(void* arg) {
+    struct UPT_info *ui = arg;
+    return ui->pid;
+}
